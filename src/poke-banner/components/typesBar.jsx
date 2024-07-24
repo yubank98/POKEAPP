9aede0b67c1ca5bef732efea9296a3cf89b82604
Michael Yubank <55451921+yubank98@users.noreply.github.com>
2024-07-24 16:04:30 -0600

    arreglos de barra de types

--- src/poke-banner/components/typesBar.jsx
+++ src/poke-banner/components/typesBar.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 import { useTypes } from "../../hooks/usetypes";
 import { getTypeIcon } from "../../utils/poke-helpers";
 import "../styles/typeBar.css";
 
 const TypeBar = ({ toggleTypes }) => {
   const types = useTypes();
+  const [selectedType, setSelectedType] = useState(null);
+
+  const handleTypeClick = (type) => {
+    setSelectedType(type);
+    toggleTypes(type);
+  };
 
   return (
     <div className="types-Bar">
@@ -12,8 +18,11 @@ const TypeBar = ({ toggleTypes }) => {
         <span
           key={type.name}
           className={`Img-type ${type.name}`}
-          onClick={() => toggleTypes(type.name)}
+          onClick={() => handleTypeClick(type.name)}
         >
+          {selectedType === type.name && (
+            <span className="typeName">{type.name}</span>
+          )}
           <img src={getTypeIcon(type.name)} alt={type.name} />
         </span>
       ))}
